4a58e7c7f287cbfa33492b64a1b956bf050dceef
Yuri van Geffen <yuri@yurivangeffen.com>
2017-06-04 13:30:20 +0200

    Fixed gradual notification.

--- Assets/Scripts/GradualNotifier.cs
+++ Assets/Scripts/GradualNotifier.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class GradualNotifier : Notifier
 {
-    public Text text;
+    public CanvasGroup text;
 	public float upperVisibleRange = 30;
 
     public override void Check(float value)
     {
         float alpha = 1f - (Mathf.Clamp(value - upperVisibleRange, 0f, 90f - upperVisibleRange) / (90f - upperVisibleRange));
-        text.color = new Color(0f, 0f, 0f, alpha);
+        text.alpha = alpha;
     }
 }
