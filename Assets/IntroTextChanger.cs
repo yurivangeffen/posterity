aa04615aa26e8960631f5d3cbafb98aeca05a3ca
Yuri van Geffen <yuri@yurivangeffen.com>
2017-06-06 14:53:27 +0200

    Removed bold as it would default to Roboto on android.

--- Assets/IntroTextChanger.cs
+++ Assets/IntroTextChanger.cs
@@ -13,13 +13,13 @@ public class IntroTextChanger : MonoBehaviour
         string dataText = "";
         if (OrientationLogger.dataType == "gradual")
         {
-            dataText = "For this experiment, you are assigned the one specific notification type: the <b> gradual notification.</b> <i> The content on the screen will "
-                + "gradually fade and become less readable</i> when the smartphone has detected that your posture could be improved.";
+            dataText = "For this experiment, you are assigned the one specific notification type: the <i>gradual notification.</i> The content on the screen will "
+                + "gradually fade and become less readable when the smartphone has detected that your posture could be improved.";
         }
         else
         {
-            dataText = "For this experiment, you are assigned the one specific notification type: the <b> instant notification.</b> <i> A notification will appear at "
-                + "the top of the screen when</i> the smartphone has detected that your posture could be improved.";
+            dataText = "For this experiment, you are assigned the one specific notification type: the <i>instant notification.</i> A notification will appear at "
+                + "the top of the screen when the smartphone has detected that your posture could be improved.";
         }
         gameObject.GetComponent<Text>().text = text.Replace("[DataText]", dataText);
 
